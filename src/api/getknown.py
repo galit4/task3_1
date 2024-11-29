8a893694417dc4dd3f234b230332a53445c4de39
Dmytro <dowlandmemes@gmail.com>
2024-11-29 23:28:00 +0200

    getknown

---
+++ src/api/getknown.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+import json
+from datetime import datetime, timedelta
+
+router = APIRouter(tags=["Get new cve"])
+
+@router.get("/get/known")
+def getting():
+    with open("/home/dmytro/Desktop/Code/under/task3/src/api/known_exploited_vulnerabilities.json", "r") as file:
+        content = json.load(file)
+        output = []
+
+  
+        for cve in content["vulnerabilities"]:         
+            for j in range(0, len(content["vulnerabilities"])):
+                startdate = str(datetime.now()-timedelta(days=j))
+                startdate = startdate.split(" ")
+                startdate = startdate[0]
+                if cve["knownRansomwareCampaignUse"] == "Known":
+                    output.append(cve)
+                    if len(output) == 10:
+                        return output
+            
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from api import info, getall, getallv2, getnew
+from api import info, getall, getallv2, getnew, getknown
 
 app = FastAPI()
 app.include_router(info.router)
 app.include_router(getall.router)
 app.include_router(getallv2.router)
 app.include_router(getnew.router)
+app.include_router(getknown.router)
