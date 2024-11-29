2b456a045aff13dd3f71e5e9235a8a9da21d492b
Dmytro <dowlandmemes@gmail.com>
2024-11-29 23:22:42 +0200

    get_new

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from api import info, getall, getallv2
+from api import info, getall, getallv2, getnew
 
 app = FastAPI()
 app.include_router(info.router)
 app.include_router(getall.router)
 app.include_router(getallv2.router)
+app.include_router(getnew.router)
