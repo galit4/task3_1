ee9094f471877a174ab46f72affb108a5de9c926
galit4 <72762160+galit4@users.noreply.github.com>
2024-12-02 11:55:53 +0200

    Update getall.py

--- src/api/getall.py
+++ src/api/getall.py
@@ -3,17 +3,19 @@ import json
 from datetime import datetime, timedelta
 
 
-
+#робимо екземляр класу та додаємо тег для зручності
 router = APIRouter(tags=["getting all cves from last 5 in records"])
 
 @router.get("/get/all")
 def getting():
+    #відкриваєм файл та зберігаємо вміст
     with open("/home/dmytro/Desktop/Code/under/task3/src/api/known_exploited_vulnerabilities.json", "r") as file:
         content = json.load(file)
         
         startdate = datetime.now()-timedelta(days=5)
-        output = []
+        output = []#пустий словник для подальшого ретурну
         for cve in content["vulnerabilities"]:
+            #якщо cve новіша за datetime.now()-timedelta(days=5), то список апендиться
             if (cve["dateAdded"] >= str(startdate)):
                 output.append(cve)
                 if len(output) == 40:
