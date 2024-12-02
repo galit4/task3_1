7c66f5d64f0271b000d7e93399da6955fefd3a62
galit4 <72762160+galit4@users.noreply.github.com>
2024-12-02 19:32:14 +0200

    Update getnew.py

--- src/api/getnew.py
+++ src/api/getnew.py
@@ -12,17 +12,18 @@ def getting():
         content = json.load(file)
         output = []#пустий словник для подальшого ретурну
 
-      #ітеруємся по усіх cve, та в  
-        for cve in content["vulnerabilities"]:         
-            for j in range(0, len(content["vulnerabilities"])):
-                startdate = str(datetime.now()-timedelta(days=j))
-                 #форматуємо вивід datetime, аби вигляд був приблизно як "2024-11-25":
+        #визначаємо 1000 днів як максимум старості цве
+        for i in range(0, 1000):
+            for cve in content["vulnerabilities"]: 
+                #визначаємо поріг часу та форматуємо його для порівняння     
+                startdate = str(datetime.now()-timedelta(days=i))
                 startdate = startdate.split(" ")
                 startdate = startdate[0]
-                
-            
-                if (cve["dateAdded"] == str(startdate)):
-                
+                #зберігаємо найновіші цве
+                if cve["dateAdded"] == startdate:
                     output.append(cve)
-                    if len(output) == 10:
-                        return output
+                #обмежуєм по кількості
+                if len(output) == 10:
+                    return output
+        return output
+            
