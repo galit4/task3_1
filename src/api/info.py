92931a5c87e45812224e48dd762e4e14dc2be253
galit4 <72762160+galit4@users.noreply.github.com>
2024-12-02 11:58:32 +0200

    Update info.py

--- src/api/info.py
+++ src/api/info.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-
+#робимо екземляр класу та додаємо тег для зручності
 router = APIRouter(tags=["Info"])
-
+#вказуєм ендпоінт в декораторі, та ретюрним інфу у функції
 @router.get("/info1")
 def info_print():
     return {"Author": "Yurchyshyn Dmytro", 
